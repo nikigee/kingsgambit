671306d137e6d0ab7954719f8874711d328ad457
Nikita <ngolev.bus@gmail.com>
2022-03-13 23:38:34 +1100

    start work on table

--- src/views/TableView.vue
+++ src/views/TableView.vue
@@ -1,16 +1,79 @@
 <template>
-  <div class="home">
-    
-  </div>
+	<div class="table">
+		<div class="monsterField">
+			<div class="monsterCard">
+				<h3>Bandit</h3>
+				<span><b>AC:</b> 12</span>
+				<span><b>Speed:</b> 30 ft.</span>
+				<span class="hpbar"><span class="hpinner">15/30</span></span>
+			</div>
+      <div class="monsterCard">
+				<h3>Bandit</h3>
+				<span><b>AC:</b> 12</span>
+				<span><b>Speed:</b> 30 ft.</span>
+				<span class="hpbar"><span class="hpinner">15/30</span></span>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 // @ is an alias to /src
 
 export default {
-  name: 'HomeView',
-  components: {
-    
-  }
-}
+	name: "TableView",
+	components: {},
+};
 </script>
+
+<style scoped>
+.table {
+	height: 100%;
+  padding-top: 50px;
+  padding-left: 10%;
+}
+.monsterCard {
+	background: rgb(238, 238, 238);
+	width: 200px;
+  height: 250px;
+  margin: 10px;
+  border-radius: 3px;
+  box-shadow: 0px 0px 5px rgb(177, 177, 177);
+  text-align: left;
+}
+.monsterCard span{
+  display: block;
+  padding: 2.5px 15px 2.5px 15px;
+  font-size: 14px;
+}
+.monsterCard h3{
+  margin: 0px;
+  padding-top: 15px;
+  padding-bottom: 5px;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+.hpbar{
+  text-align: center;
+  background: #1a1a1a;
+  margin-left: 15px;
+  margin-right: 15px;
+  padding: 0px !important;
+  border-radius: 3px;
+  overflow: hidden;
+  color: white;
+  margin-top: 5px;
+}
+.hpinner{
+  width: 50%;
+  background: green;
+}
+.monsterField {
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	justify-content: flex-start;
+  align-content: flex-start;
+  height: 100%;
+}
+</style>
